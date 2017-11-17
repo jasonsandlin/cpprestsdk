9d6eba32ce40622073c324545bac39657a9988ed
Jason Sandlin <jasonsa@microsoft.com>
2017-11-16 16:45:42 -0800

    Fixing script bug

--- MakeJsonStandalone.cmd
+++ MakeJsonStandalone.cmd
@@ -8,6 +8,7 @@ mkdir %DESTFOLDER%\details
 copy %SRCFOLDER%\Release\include\cpprest\json.h %DESTFOLDER%\json.h
 copy %SRCFOLDER%\Release\include\cpprest\asyncrt_utils.h %DESTFOLDER%\details\asyncrt_utils.h
 copy %SRCFOLDER%\Release\include\cpprest\details\SafeInt3.hpp %DESTFOLDER%\details\SafeInt3.hpp
+copy %SRCFOLDER%\Release\include\cpprest\details\cpprest_compat.h %DESTFOLDER%\details\cpprest_compat.h
 copy %SRCFOLDER%\Release\src\utilities\asyncrt_utils.cpp %DESTFOLDER%\details\asyncrt_utils.hpp
 copy %SRCFOLDER%\Release\src\json\json.cpp %DESTFOLDER%\details\json.hpp
 copy %SRCFOLDER%\Release\src\json\json_parsing.cpp %DESTFOLDER%\details\json_parsing.hpp
